8cdd6d1f4073451a8e874768fbf30c5fee926cf4
Jimmy Tai <jimmytai0315@hotmail.com>
2022-08-09 18:38:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# klint_massive_file_downloader
+ This is an Android App which keeps downloading file in background. It is a test project for Qualcomm 5G chip.
---
+++ android/app/src/main/kotlin/dev/jimmytai/klint/downloader/klint_massive_file_downloader/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.jimmytai.klint.downloader.klint_massive_file_downloader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
